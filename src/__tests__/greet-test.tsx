439dd5cbcc8d63c7b8844768ff73e5b0d77bb8ad
Javier Marcon <javiermarcon@gmail.com>
2024-05-28 16:43:19 -0300

    convenciones

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,21 @@
+/**
+ * Greet should render the text hello and if a name is passed into the component
+ * it should render hello followed by the name
+ */
+
+import { render, screen } from '@testing-library/react'
+import { Greet } from '../components/greet/greet'
+
+describe('Greet', () => {
+    it('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    it('renders with a name', () => {
+        render(<Greet name='Javo' />)
+        const textElement = screen.getByText('Hello Javo')
+        expect(textElement).toBeInTheDocument()
+    })
+})
