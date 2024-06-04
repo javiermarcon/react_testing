d976ab23e84f7401c5c85a0a33e1a911622b7dd6
Javier Marcon <javiermarcon@gmail.com>
2024-06-04 15:07:55 -0300

    Mocking functions

---
+++ src/components/counter-two/counter-two.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { CounterTwo } from "./counter-two";
+import user from "@testing-library/user-event"
+
+describe("CounterTwo", () => {
+    test("renders correctly", () => {
+        render(<CounterTwo count={0} />);
+        const textElement = screen.getByText("Counter Two");
+        expect(textElement).toBeInTheDocument();
+    });
+
+    test("handlers are called", async () => {
+        user.setup()
+        const incrementHanler = jest.fn()
+        const decrementHandler = jest.fn()
+        render(<CounterTwo count={0}
+            handleIncrement={incrementHanler}
+            handleDecrement={decrementHandler}
+        />);
+        const incrementButton = screen.getByRole('button', { name: 'Increment' })
+        const decrementButton = screen.getByRole('button', { name: 'Decrement' })
+        await user.click(incrementButton)
+        await user.click(decrementButton)
+        expect(incrementHanler).toHaveBeenCalledTimes(1)
+        expect(decrementHandler).toHaveBeenCalledTimes(1)
+    })
+});
