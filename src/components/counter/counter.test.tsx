9a4c2bbc62b8c7dcbc4403d8ca497a0d1f516886
Javier Marcon <javiermarcon@gmail.com>
2024-06-04 00:18:28 -0300

    Keyboard interactions

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -19,7 +19,7 @@ describe("Counter", () => {
         expect(counterElement).toHaveTextContent("0")
     })
 
-    test('renders a count of 1 after clicking te increment button', async () => {
+    test('renders a count of 1 after clicking the increment button', async () => {
         user.setup()
         render(<Counter />)
         const incrementButton = screen.getByRole('button', {
@@ -30,7 +30,7 @@ describe("Counter", () => {
         expect(counterElement).toHaveTextContent("1")
     })
 
-    test('renders a count of 2 after clicking te increment button twice', async () => {
+    test('renders a count of 2 after clicking the increment button twice', async () => {
         user.setup()
         render(<Counter />)
         const incrementButton = screen.getByRole('button', {
@@ -40,5 +40,37 @@ describe("Counter", () => {
         await user.click(incrementButton)
         const counterElement = screen.getByRole('heading')
         expect(counterElement).toHaveTextContent("2")
+    })
+
+    test('renders a count of 10 after clicking the set button', async () => {
+        user.setup()
+        render(<Counter />)
+        const amountInput = screen.getByRole('spinbutton') // para input type number
+        await user.type(amountInput, '10')
+        expect(amountInput).toHaveValue(10)
+        const setButton = screen.getByRole('button', {
+            name: 'Set',
+        })
+        await user.click(setButton)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent("10")
+    })
+
+    test('elements are focused in the right order', async () => {
+        user.setup()
+        render(<Counter />)
+        const amountInput = screen.getByRole('spinbutton')
+        const setButton = screen.getByRole('button', {
+            name: 'Set',
+        })
+        const incrementButton = screen.getByRole('button', {
+            name: 'Increment'
+        })
+        await user.tab()
+        expect(incrementButton).toHaveFocus()
+        await user.tab()
+        expect(amountInput).toHaveFocus()
+        await user.tab()
+        expect(setButton).toHaveFocus()
     })
 })
