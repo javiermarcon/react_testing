5ea226cf6fd592f087755e058337d9c23b6858e2
Javier Marcon <javiermarcon@gmail.com>
2024-06-04 00:56:06 -0300

    Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import './App.css';
-import { Counter } from './components/counter/counter';
-// import { Application } from './components/application/application';
-// import { Skills } from './components/skills/skills';
+import { AppProviders } from './providers/app-providers';
+import { MuiMode } from "./components/mui/mui-mode";
 
 function App() {
-  // const skills = ["HTML", "CSS", "JavaScript"];
-  
   return (
-    <div className="App">
-      {/* <Application /> */}
-      {/* <Skills skills={skills} /> */}
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
