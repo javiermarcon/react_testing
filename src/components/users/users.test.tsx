feeaf1fb5e1cacf8559211ee06d9ffc223b7eb80
Javier Marcon <javiermarcon@gmail.com>
2024-06-07 13:03:40 -0300

    MSW Error Handling

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/react";
 import { Users } from "./users";
 import { server } from '../../mocks/server'
 import { describe, test } from 'vitest';
+import { http, HttpResponse } from "msw";
 
 describe("Users", () => {
-    server.listen()
     test("renders correctly", () => {
         render(<Users />);
         const textElement = screen.getByText("Users");
@@ -12,8 +12,22 @@ describe("Users", () => {
     });
 
     test('renders a list of users', async () => {
-        render(<Users />)
+        render(<Users />);
         const users = await screen.findAllByRole('listitem')
         expect(users).toHaveLength(3)
     })
+
+    test("renders error", async () => {
+        server.use(
+            http.get("https://jsonplaceholder.typicode.com/users", () => {
+                return new HttpResponse(null, {
+                    status: 500,
+                    statusText: 'Internal server error',
+                  })
+                }),
+            )
+        render(<Users />);
+        const error = await screen.findByText('Error fetching users');
+        expect(error).toBeInTheDocument();
+        })
 })
